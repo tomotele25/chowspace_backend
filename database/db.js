a5571fe6dc044d8a61ae08034815b104797812be
tomotele <tomotelechristopher25@gmail.com>
2025-07-01 02:23:26 +0100

    :Testing payment Flutterwave

--- database/db.js
+++ database/db.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const connectToDb = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       connectTimeoutMS: 10000,
       socketTimeoutMS: 45000,
     });
