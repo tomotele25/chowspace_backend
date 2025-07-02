1ac1fb88b3a66f0d0d8f5e75ae6718e9ceff8ab0
tomotele <tomotelechristopher25@gmail.com>
2025-07-02 22:40:30 +0100

    :Add locations

--- database/db.js
+++ database/db.js
@@ -1,14 +1,17 @@
-require("dotenv").config();
+import mongoose from "mongoose";
 
-const mongoose = require("mongoose");
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
 
-const connectToDb = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
   }
 };
 
-module.exports = connectToDb;
+export default connectDB;
